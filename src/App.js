1e894e6655408eadd3e8ad0a22807913c4c51384
Dreamcode <info@dreamcode.io>
2018-11-19 13:49:30 -0500

    testing push on netlify 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>just testing !!!</p>
-          <p>
+          {/* <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
@@ -19,7 +19,7 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
