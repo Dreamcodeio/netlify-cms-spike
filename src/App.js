ac1500d53790d48ea798385e7481460f9f53a916
Dreamcode <info@dreamcode.io>
2018-11-20 14:37:39 -0500

    modified prismic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import { asyncImport } from "./helpers";
+import Product from "./product";
 
 import "./assets/stylesheets/main.css";
-
-// const Product = asyncImport(import("./product"));
 
 class App extends Component {
   render() {
@@ -14,18 +12,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>just testing !!!</p>
-          {/* <Product /> */}
-          {/* <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
+          <Product />
         </header>
       </div>
     );
--- src/product.js
+++ src/product.js
@@ -1,36 +1,78 @@
-import React from "react";
-import { RichText, Date, Link } from "prismic-reactjs";
-import { Client, linkResolver } from "./prismic";
+import React, { Component } from "react";
+import Prismic from "prismic-javascript";
+import { Link, RichText, Date } from "prismic-reactjs";
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+class Product extends Component {
+  state = {
+    doc: null
+  };
+
+  // Link Resolver
+  linkResolver(doc) {
+    // Define the url depending on the document type
+    if (doc.type === "page") {
+      return "/page/" + doc.uid;
+    } else if (doc.type === "blog_post") {
+      return "/blog/" + doc.uid;
+    }
+
+    // Default to homepage
+    return "/";
   }
 
-  componentDidUpdate() {
-    if (this.state.product) window.prerenderReady = true;
-    console.log("componentDidUpdate::", this.state);
-  }
+  componentWillMount() {
+    const apiEndpoint = "https://netlify-cms-spike.prismic.io/api/v2";
 
-  componentDidMount() {
-    Client.getByUID("product", this.props.computedMatch.params.uid)
-      .then(product => this.setState({ product }))
-      .catch(error => console.error(error));
-
-    console.log("componentDidMount::", this.state);
+    Prismic.api(apiEndpoint).then(api => {
+      api
+        .query(Prismic.Predicates.at("document.type", "page"))
+        .then(response => {
+          if (response) {
+            this.setState({ doc: response.results[0] });
+          }
+        });
+    });
   }
 
   render() {
-    return (
-      <div>
-        {RichText.render(this.state.product.data.product_name, linkResolver)}
-        {RichText.render(
-          this.state.product.data.product_description,
-          linkResolver
-        )}
-        <img src={this.state.product.data.product_illustration.url} />
-      </div>
-    );
+    if (this.state.doc) {
+      const document = this.state.doc.data;
+      return (
+        <div>
+          <h1>{RichText.asText(document.title)}</h1>
+          <img alt="cover" src={document.image.url} />
+          {RichText.render(document.description, this.linkResolver)}
+        </div>
+      );
+    }
+    return <h1>Loading...</h1>;
   }
+
+  // componentDidUpdate() {
+  //   if (this.state.product) window.prerenderReady = true;
+  //   console.log("componentDidUpdate::", this.state);
+  // }
+
+  // componentDidMount() {
+  //   Client.getByUID("product", this.props.computedMatch.params.uid)
+  //     .then(product => this.setState({ product }))
+  //     .catch(error => console.error(error));
+
+  //   console.log("componentDidMount::", this.state);
+  // }
+
+  // render() {
+  //   return (
+  //     <div>
+  //       {RichText.render(this.state.product.data.product_name, linkResolver)}
+  //       {RichText.render(
+  //         this.state.product.data.product_description,
+  //         linkResolver
+  //       )}
+  //       <img src={this.state.product.data.product_illustration.url} />
+  //     </div>
+  //   );
+  // }
 }
+
+export default Product;
